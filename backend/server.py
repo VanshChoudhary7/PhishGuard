40e8eeab9aaf76b62e1120c204241026a00f1b6d
Vansh Choudhary <Iamvanshchaudhary7@gmail.com>
2026-07-03 17:44:16 +0530

    fix : Vercel URL

--- backend/server.py
+++ backend/server.py
@@ -10,7 +10,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), 'ml'))
 from feature_extractor import extract_features
 
 app = Flask(__name__)
-CORS(app, origins=["https://phish-gaurd-one.vercel.app"])
+CORS(app, origins=["https://phish-guard-black-mu.vercel.app"])
 
 # Load model and datasets
 print("Loading model...")
